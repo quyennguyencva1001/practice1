80ded69c01e4d9d1c878eddf5030c9d11780bb50
quyennguyencva1001 <quyennguyencva1001@gmail.com>
2024-06-19 22:49:13 +0700

    add 2 py

---
+++ tim_so_nguyen_to.py
@@ -0,0 +1,13 @@
+def nguyen_to(x):
+    b=0
+    for i in range (2,x):
+        if x%i==0:
+            b=b+1
+            if b==1:
+                break   
+    if b==0:
+        print(str(x)+ " " + "la so nguyen to")
+    else:
+        print(str(x)+ " " + "khong la so nguyen to")
+ket_qua= nguyen_to(15)
+print(ket_qua)
---
+++ ve_tam_giac.py
@@ -0,0 +1,5 @@
+a=int(input("nhap a"))
+for j in (a+1,1,-2):
+        print(" "*j)
+        for i in range (1,a+1,2):
+            print("*"*i)
