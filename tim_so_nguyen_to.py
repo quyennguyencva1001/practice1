58f5b01b0f5222d2e2ec800915d964edf00d5544
quyennguyencva1001 <quyennguyencva1001@gmail.com>
2024-06-21 21:14:43 +0700

    kiem tra ca day so nguyen to

--- tim_so_nguyen_to.py
+++ tim_so_nguyen_to.py
@@ -1,4 +1,18 @@
 
+# def nguyen_to(x):
+#     b=0
+#     for i in range (2,x):
+#         if x%i==0:
+#             b=b+1
+#             if b==1:
+#                 break   
+#     if b==0:
+#         print(str(x)+ " " + "la so nguyen to")
+#     else:
+#         print(str(x)+ " " + "khong la so nguyen to")
+# ket_qua= nguyen_to(15)
+# print(ket_qua)
+#return True false, tạo list đếm số nt trong list
 def nguyen_to(x):
     b=0
     for i in range (2,x):
@@ -7,9 +21,12 @@ def nguyen_to(x):
             if b==1:
                 break   
     if b==0:
-        print(str(x)+ " " + "la so nguyen to")
+        return True
     else:
-        print(str(x)+ " " + "khong la so nguyen to")
-ket_qua= nguyen_to(15)
-print(ket_qua)
-#return True false, tạo list đếm số nt trong list
+        return False
+a=[2,3,4,5]
+c=0
+for j in a:
+    if nguyen_to(j)==True:
+        c=c+1
+print("Co " + str(c)+" so nguyen to")
